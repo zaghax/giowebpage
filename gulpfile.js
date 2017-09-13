18d91712e84c460166abd01cefe86d335c44a370
Giovanny Ortega Arias <gortegaa@caracoltv.com.co>
2017-09-13 14:20:21 -0500

    Avances Gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var  autoprefixer = require('autoprefixer'),
+     browserSync  = require('browser-sync'),
+     concat       = require('gulp-concat'),
+     gulp         = require('gulp'),
+     postcss      = require('gulp-postcss'),
+     sass         = require('gulp-sass'),
+     // sourcemaps   = require('gulp-sourcemaps'),
+     uglify       = require('gulp-uglify');
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('sass', function(){
+  gulp.src(['sass/**/*.sass'])
+      .pipe(sass({ 
+        outputStyle: 'expanded',
+        sourceComments: 'normal'
+      }))
+      .on('error', handleError)
+      // .pipe(sourcemaps.init())
+      .pipe(postcss([ autoprefixer({browsers: ['last 2 versions'] }) ]))
+      // .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('css/'))
+      .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js', function(){
+  return gulp.src(['srcjs/jquery.js'])
+             .pipe(concat('main.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('js/'))
+             .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch(['sass/**/*.sass'], ['sass']);
+  gulp.watch(['srcjs/**/*.js'], ['js']);
+  gulp.watch("*.html", ['bs-reload']);
+});
