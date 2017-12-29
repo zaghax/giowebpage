dc12f8f484a33a5c25a4df55432f00715fb2622d
Giovanny Ortega Arias <gortegaa@caracoltv.com.co>
2017-12-29 11:28:01 -0500

    Avances varios

--- srcjs/custom.js
+++ srcjs/custom.js
@@ -1,2 +1,21 @@
-console.log('mi perro')
+
+
+$(document).ready(function(){
+
+	$('.menu ul li a').click(function(){
+		$('.logo-opening').addClass('active');
+	});
+
+	$('.menu ul li .home').click(function(){
+		$('.logo-opening').removeClass('active');
+		$('.page.work').removeClass('active');
+	});
+
+	$('.menu ul li .work').click(function(){
+		console.log('active')
+		$('.page.work').addClass('active');
+	});
+
+
+});
 
